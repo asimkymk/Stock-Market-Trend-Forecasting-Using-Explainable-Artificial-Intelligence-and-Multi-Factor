cdf848b271fcae8d527dc37fc291b20b7c2ac0bf
Asım Kaymak <kaymakasm@gmail.com>
2023-04-06 17:18:19 +0300

    handling nonetype error

--- data/temp.py
+++ data/temp.py
@@ -9,27 +9,7 @@ nltk.download('punkt')
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 config = Config()
 config.browser_user_agent = user_agent
-googlenews=GoogleNews(start='01/01/2023',end='01/01/2023')
-googlenews.search('AAPL')
+googlenews=GoogleNews(start='03/04/2022',end='03/04/2022')
+googlenews.search('AGNC')
 result=googlenews.result()
-df=pd.DataFrame(result)
-print(df.head())
-for i in range(2,3):
-    googlenews.getpage(i)
-    result=googlenews.result()
-    df=pd.DataFrame(result)
-list=[]
-for ind in df.index:
-    dict={}
-    article = Article(df['link'][ind],config=config)
-    article.download()
-    article.parse()
-    article.nlp()
-    dict['Date']=df['date'][ind]
-    dict['Media']=df['media'][ind]
-    dict['Title']=article.title
-    dict['Article']=article.text
-    dict['Summary']=article.summary
-    list.append(dict)
-news_df=pd.DataFrame(list)
-news_df.to_excel("articles.xlsx")
+print(result)
