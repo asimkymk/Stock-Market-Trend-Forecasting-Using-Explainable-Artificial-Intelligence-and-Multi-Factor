4ad49f011bf92362b98e18eefa79941f66022c3a
Asım Kaymak <kaymakasm@gmail.com>
2023-04-09 16:42:28 +0300

    news and trands data continue

--- data/google_trends/data_scraper.js
+++ data/google_trends/data_scraper.js
@@ -9,7 +9,19 @@ const tickers = [
     'RPHM', 'SCHW', 'SNAP', 'TEAF', 'TSLA', 'UFAB', 'ULBI', 'VALE', 'XPEV', 'XTNT', 'YCBD'
 ];
 
-const dateIntervals = [{ startDate: '2022-03-01', endDate: '2022-03-31' }, { startDate: '2022-04-01', endDate: '2022-04-30' }, { startDate: '2022-05-01', endDate: '2022-05-31' }, { startDate: '2022-06-01', endDate: '2022-06-30' }, { startDate: '2022-07-01', endDate: '2022-07-31' }, { startDate: '2022-08-01', endDate: '2022-08-31' }, { startDate: '2022-09-01', endDate: '2022-09-30' }, { startDate: '2022-10-01', endDate: '2022-10-31' }, { startDate: '2022-11-01', endDate: '2022-11-30' }, { startDate: '2022-12-01', endDate: '2022-12-31' }, { startDate: '2023-01-01', endDate: '2023-01-31' }, { startDate: '2023-02-01', endDate: '2023-02-28' }, { startDate: '2023-03-01', endDate: '2023-04-03' }]
+const dateIntervals = [{ startDate: '2022-03-01', endDate: '2022-03-31' },
+                     { startDate: '2022-04-01', endDate: '2022-04-30' }, 
+                     { startDate: '2022-05-01', endDate: '2022-05-31' }, 
+                     { startDate: '2022-06-01', endDate: '2022-06-30' }, 
+                     { startDate: '2022-07-01', endDate: '2022-07-31' }, 
+                     { startDate: '2022-08-01', endDate: '2022-08-31' }, 
+                     { startDate: '2022-09-01', endDate: '2022-09-30' }, 
+                     { startDate: '2022-10-01', endDate: '2022-10-31' }, 
+                     { startDate: '2022-11-01', endDate: '2022-11-30' }, 
+                     { startDate: '2022-12-01', endDate: '2022-12-31' }, 
+                     { startDate: '2023-01-01', endDate: '2023-01-31' }, 
+                     { startDate: '2023-02-01', endDate: '2023-02-28' }, 
+                     { startDate: '2023-03-01', endDate: '2023-04-03' }]
     ;
 
 async function fetchTrendData(keyword, startDate, endDate) {
@@ -40,24 +52,24 @@ async function fetchAllTrends(tickers, dateIntervals) {
 
     for (let ticker of tickers) {
         try {
-            for (const interval of dateIntervals) {
-                await sleep(1000);
-                console.log(`Fetching data for ${ticker} between ${interval.startDate} and ${interval.endDate}...`);
-                let search = "NASDAQ:" + ticker
-                const trendData = await fetchTrendData(search, interval.startDate, interval.endDate);
-                console.log(search);
-                const formattedData = trendData.map((item) => {
-                    return {
-                        keyword: ticker,
-                        time: formatDate(item.time * 1000),
-                        value: item.value[0],
-                    };
-                });
+            interval = dateIntervals[0]
+            console.log(`Fetching data for ${ticker} between ${interval.startDate} and ${interval.endDate}...`);
+            let search = "NASDAQ:" + ticker
+            const trendData = await fetchTrendData(search, interval.startDate, interval.endDate);
+            console.log(search);
+            const formattedData = trendData.map((item) => {
+                return {
+                    keyword: ticker,
+                    time: formatDate(item.time * 1000),
+                    value: item.value[0],
+                };
+            });
 
-                allTrends = allTrends.concat(formattedData);
-            }
-            await sleep(15000);
+            allTrends = allTrends.concat(formattedData);
+        
+        
         } catch (error) {
+            console.log(error)
             return allTrends;
         }
 
@@ -75,7 +87,7 @@ async function main() {
     const csvData = json2csvParser.parse(allTrends);
 
     // CSV verisini dosyaya yaz
-    fs.writeFile('all_trends.csv', csvData, (err) => {
+    fs.appendFile('all_trends.csv', csvData, (err) => {
         if (err) {
             console.error('Hata:', err);
         } else {
