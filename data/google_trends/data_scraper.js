04df2c208192a55c253510da2eeea2be7434f900
Asım Kaymak <kaymakasm@gmail.com>
2023-04-09 05:53:11 +0300

    some changes

--- data/google_trends/data_scraper.js
+++ data/google_trends/data_scraper.js
@@ -9,23 +9,14 @@ const tickers = [
     'RPHM', 'SCHW', 'SNAP', 'TEAF', 'TSLA', 'UFAB', 'ULBI', 'VALE', 'XPEV', 'XTNT', 'YCBD'
 ];
 
-const dateIntervals = [
-    {
-        startDate: '2022-03-01',
-        endDate: '2022-09-01',
-    },
-    {
-        startDate: '2022-09-02',
-        endDate: '2023-04-03',
-    },
-];
+const dateIntervals = [{ startDate: '2022-03-01', endDate: '2022-03-31' }, { startDate: '2022-04-01', endDate: '2022-04-30' }, { startDate: '2022-05-01', endDate: '2022-05-31' }, { startDate: '2022-06-01', endDate: '2022-06-30' }, { startDate: '2022-07-01', endDate: '2022-07-31' }, { startDate: '2022-08-01', endDate: '2022-08-31' }, { startDate: '2022-09-01', endDate: '2022-09-30' }, { startDate: '2022-10-01', endDate: '2022-10-31' }, { startDate: '2022-11-01', endDate: '2022-11-30' }, { startDate: '2022-12-01', endDate: '2022-12-31' }, { startDate: '2023-01-01', endDate: '2023-01-31' }, { startDate: '2023-02-01', endDate: '2023-02-28' }, { startDate: '2023-03-01', endDate: '2023-04-03' }]
+    ;
 
 async function fetchTrendData(keyword, startDate, endDate) {
     const result = await googleTrends.interestOverTime({
         keyword,
         startTime: new Date(startDate),
-        endTime: new Date(endDate),
-        geo: '', // Dünya genelinde veri almak için geo parametresini boş bir string olarak ayarla
+        endTime: new Date(endDate) // Dünya genelinde veri almak için geo parametresini boş bir string olarak ayarla
     });
 
     return JSON.parse(result).default.timelineData;
@@ -42,29 +33,35 @@ function formatDate(timestamp) {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
 
 async function fetchAllTrends(tickers, dateIntervals) {
     let allTrends = [];
 
     for (let ticker of tickers) {
-        await sleep(3000);
-        for (const interval of dateIntervals) {
-            console.log(`Fetching data for ${ticker} between ${interval.startDate} and ${interval.endDate}...`);
-            let search = "NASDAQ:" + ticker
-            const trendData = await fetchTrendData(search, interval.startDate, interval.endDate);
-            console.log(search);
-            const formattedData = trendData.map((item) => {
-                return {
-                    keyword: ticker,
-                    time: formatDate(item.time * 1000),
-                    value: item.value[0],
-                };
-            });
+        try {
+            for (const interval of dateIntervals) {
+                await sleep(1000);
+                console.log(`Fetching data for ${ticker} between ${interval.startDate} and ${interval.endDate}...`);
+                let search = "NASDAQ:" + ticker
+                const trendData = await fetchTrendData(search, interval.startDate, interval.endDate);
+                console.log(search);
+                const formattedData = trendData.map((item) => {
+                    return {
+                        keyword: ticker,
+                        time: formatDate(item.time * 1000),
+                        value: item.value[0],
+                    };
+                });
 
-            allTrends = allTrends.concat(formattedData);
+                allTrends = allTrends.concat(formattedData);
+            }
+            await sleep(15000);
+        } catch (error) {
+            return allTrends;
         }
-        
+
+
     }
 
     return allTrends;
