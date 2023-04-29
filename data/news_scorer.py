5e7885bf3d6f876474d60ebd2521524b53791c3d
Asım Kaymak <kaymakasm@gmail.com>
2023-04-29 04:58:07 +0300

    news scorer updated and create_daily_news_score function created

--- data/news_scorer.py
+++ data/news_scorer.py
@@ -42,78 +42,70 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from textblob import TextBlob
 import pandas as pd
-class NewsScorer:
 
-    def __init__(self,):
-        self.__news_data = ""
-        self.__scored_data = ""
+
+class NewsScorer:
+    def __init__(self):
+        self.news_data = pd.DataFrame()
+        self.scored_data = pd.DataFrame()
         nltk.download('punkt')
         nltk.download('stopwords')
         nltk.download('wordnet')
+        nltk.download('averaged_perceptron_tagger')
+        nltk.download('brown')
 
-    def set_news_data(self,news_data):
-        self.__news_data = news_data
-    
-    def get_news_data(self):
-        return self.__news_data
-    
-    def set_scored_data(self,scored_data):
-        self.__scored_data = scored_data
-    
-    def get_scored_data(self):
-        self.__scored_data
+    def set_news_data(self, news_data):
+        self.news_data = news_data
 
-    def create_scored_data(self,):
+    def create_scored_data(self):
         words = []
-        for text in self.__news_data['text']:
-            tokens = word_tokenize(text)
-            
+        for text in self.news_data['text']:
+            tokens = word_tokenize(text.lower())
             tokens = [word for word in tokens if word.isalpha() and word not in stopwords.words('english')]
-            
             lemmatizer = WordNetLemmatizer()
             tokens = [lemmatizer.lemmatize(word) for word in tokens]
-            
             words += tokens
-
 
         unique_words = set(words)
 
-
         sentiments = []
-        for text in self.__news_data['text']:
+        for text in self.news_data['text']:
             blob = TextBlob(text)
             sentiment = blob.sentiment.polarity
             sentiments.append(sentiment)
 
-
         scores = []
-        for text, sentiment in zip(self.__news_data['text'], sentiments):
-            blob = TextBlob(text)
-
-            title_tokens = word_tokenize(self.__news_data['title'][0])
-            text_tokens = word_tokenize(text)
-
+        for text, sentiment in zip(self.news_data['text'], sentiments):
+            blob = TextBlob(text.lower())
+            title_tokens = word_tokenize(self.news_data['title'][0].lower())
+            text_tokens = word_tokenize(text.lower())
             title_tokens = [lemmatizer.lemmatize(word) for word in title_tokens]
             text_tokens = [lemmatizer.lemmatize(word) for word in text_tokens]
-
             title_unique_words = set(title_tokens)
             text_unique_words = set(text_tokens)
-
             shared_title_words = title_unique_words.intersection(unique_words)
             shared_text_words = text_unique_words.intersection(unique_words)
-
             title_score = sum([sentiment * blob.words.count(word) for word in shared_title_words])
             text_score = sum([sentiment * blob.words.count(word) for word in shared_text_words])
-
             score = title_score + text_score
             scores.append(score)
 
-        self.__scored_data = self.__news_data
-        self.__scored_data['news_score'] = scores
+        self.scored_data = self.news_data.copy()
+        self.scored_data['news_score'] = scores
 
     def get_average_score(self):
-        daily_mean = self.__scored_data.groupby('date')['news_score'].mean()
+        daily_mean = self.scored_data.groupby('date')['news_score'].mean()
+        print(daily_mean)
         return daily_mean
+
+    
+    def sentiment_analysis(self, news_text):
+        analysis = TextBlob(news_text)
+        
+        # Polarity: -1 (olumsuz) ile 1 (olumlu) arasında bir değer alır.
+        return analysis.sentiment.polarity
+        
+        
 
         
     
--- main.py
+++ main.py
@@ -7,80 +7,82 @@ from datetime import datetime
 import os
 from newspaper import Article
 from data.news_scraper import get_detailed_text_data
+from data.news_scorer import NewsScorer
+
 # TODO
 # Tüm haberler çekilecek.
 # Code refactoring.
 
 longNames = {
-    'AAL': 'American Airlines',
-    'AAPL': 'Apple',
-    'AGNC': 'AGNC Investment',
-    'AMC': 'AMC Entertainment',
-    'AMD': 'Advanced Micro Devices',
-    'AMZN': 'Amazon',
-    'ASML': 'ASML Holding',
-    'ATKR': 'Atkore',
-    'BAC': 'Bank of America',
-    'BBBY': 'Bed Bath & Beyond',
-    'BBIO': 'BridgeBio Pharma',
-    'BLU': 'BELLUS Health',
-    'BMEA': 'Biomea Fusion',
-    'BSGA': 'Blue Safari Group Acquisition',
-    'CSCO': 'Cisco',
-    'CTRA': 'Coterra Energy',
-    'DKNG': 'DraftKings',
-    'ELYM': 'Eliem Therapeutics',
-    'ERIC': 'Ericsson',
-    'ETRN': 'Equitrans Midstream',
-    'EXTR': 'Extreme Networks',
-    'FDBC': 'Fidelity D&D Bancorp',
-    'FRC': 'First Republic Bank',
-    'GDEN': 'Golden Entertainment',
-    'GMDA': 'Gamida Cell',
-    'GMVD': 'G Medical Innovations',
-    'GNRC': 'Generac',
-    'GOOG': 'Alphabet',
-    'GRAB': 'Grab Holdings',
-    'GRIN': 'Grindrod Shipping Holdings',
-    'HAIA': 'Healthcare AI Acquisition',
-    'HBAN': 'Huntington Bancshares',
-    'HLMN': 'Hillman Solutions',
-    'HSAI': 'Hesai Group',
-    'HWCPZ': 'Hancock Whitney',
-    'HYFM': 'Hydrofarm Holdings',
-    'IMAQ': 'International Media Acquisition',
-    'INTC': 'Intel',
-    'IRMD': 'iRadimed',
-    'ISRG': 'Intuitive Surgical',
-    'JBLU': 'JetBlue Airways',
-    'LCID': 'Lucid Group',
-    'LUNR': 'Intuitive Machines',
-    'MRVL': 'Marvell Technology',
-    'MSFT': 'Microsoft',
-    'NFLX': 'Netflix',
-    'NIO': 'Nio',
-    'NVDA': 'Nvidia',
-    'PACW': 'PacWest Bancorp',
-    'PHYS': 'Sprott Physical Gold Trust',
-    'PSTX': 'Poseida Therapeutics',
-    'RBLX': 'Roblox',
-    'RIVN': 'Rivian',
-    'ROKU': 'Roku',
-    'RPHM': 'Reneo Pharmaceuticals',
-    'SCHW': 'Charles Schwab',
-    'SGHT': 'Sight Sciences',
-    'SNAP': 'Snap',
-    'STRO': 'Sutro Biopharma',
-    'TEAF': 'Ecofin Sustainble',
-    'TSLA': 'Tesla',
-    'UAL': 'United Airlines',
-    'UFAB': 'Unique Fabricating',
-    'ULBI': 'Ultralife',
-    'VALE': 'Vale',
-    'WAL': 'Western Alliance',
-    'XPEV': 'XPeng',
-    'XTNT': 'Xtant Medical',
-    'YCBD': 'cbdMD'
+    "AAL": "American Airlines",
+    "AAPL": "Apple",
+    "AGNC": "AGNC Investment",
+    "AMC": "AMC Entertainment",
+    "AMD": "Advanced Micro Devices",
+    "AMZN": "Amazon",
+    "ASML": "ASML Holding",
+    "ATKR": "Atkore",
+    "BAC": "Bank of America",
+    "BBBY": "Bed Bath & Beyond",
+    "BBIO": "BridgeBio Pharma",
+    "BLU": "BELLUS Health",
+    "BMEA": "Biomea Fusion",
+    "BSGA": "Blue Safari Group Acquisition",
+    "CSCO": "Cisco",
+    "CTRA": "Coterra Energy",
+    "DKNG": "DraftKings",
+    "ELYM": "Eliem Therapeutics",
+    "ERIC": "Ericsson",
+    "ETRN": "Equitrans Midstream",
+    "EXTR": "Extreme Networks",
+    "FDBC": "Fidelity D&D Bancorp",
+    "FRC": "First Republic Bank",
+    "GDEN": "Golden Entertainment",
+    "GMDA": "Gamida Cell",
+    "GMVD": "G Medical Innovations",
+    "GNRC": "Generac",
+    "GOOG": "Alphabet",
+    "GRAB": "Grab Holdings",
+    "GRIN": "Grindrod Shipping Holdings",
+    "HAIA": "Healthcare AI Acquisition",
+    "HBAN": "Huntington Bancshares",
+    "HLMN": "Hillman Solutions",
+    "HSAI": "Hesai Group",
+    "HWCPZ": "Hancock Whitney",
+    "HYFM": "Hydrofarm Holdings",
+    "IMAQ": "International Media Acquisition",
+    "INTC": "Intel",
+    "IRMD": "iRadimed",
+    "ISRG": "Intuitive Surgical",
+    "JBLU": "JetBlue Airways",
+    "LCID": "Lucid Group",
+    "LUNR": "Intuitive Machines",
+    "MRVL": "Marvell Technology",
+    "MSFT": "Microsoft",
+    "NFLX": "Netflix",
+    "NIO": "Nio",
+    "NVDA": "Nvidia",
+    "PACW": "PacWest Bancorp",
+    "PHYS": "Sprott Physical Gold Trust",
+    "PSTX": "Poseida Therapeutics",
+    "RBLX": "Roblox",
+    "RIVN": "Rivian",
+    "ROKU": "Roku",
+    "RPHM": "Reneo Pharmaceuticals",
+    "SCHW": "Charles Schwab",
+    "SGHT": "Sight Sciences",
+    "SNAP": "Snap",
+    "STRO": "Sutro Biopharma",
+    "TEAF": "Ecofin Sustainble",
+    "TSLA": "Tesla",
+    "UAL": "United Airlines",
+    "UFAB": "Unique Fabricating",
+    "ULBI": "Ultralife",
+    "VALE": "Vale",
+    "WAL": "Western Alliance",
+    "XPEV": "XPeng",
+    "XTNT": "Xtant Medical",
+    "YCBD": "cbdMD",
 }
 
 
@@ -88,11 +90,15 @@ def create_new_tickers_data():
     # tickers = ['AAL', 'AAPL', 'AGNC', 'AMC', 'AMD', 'AMZN', 'ASML', 'ATKR', 'BAC', 'BBBY', 'BBIO', 'BLU', 'BMEA', 'BSGA', 'CSCO', 'CTRA', 'DKNG', 'ELYM', 'ERIC', 'ETRN', 'EXTR', 'FDBC', 'FRC', 'GDEN', 'GMDA', 'GMVD', 'GNRC', 'GOOG', 'GRAB', 'GRIN', 'HAIA', 'HBAN', 'HLMN', 'HSAI',
     #          'HWCPZ', 'HYFM', 'IMAQ', 'INTC', 'IRMD', 'ISRG', 'JBLU', 'LCID', 'LUNR', 'MRVL', 'MSFT', 'NFLX', 'NIO', 'NVDA', 'PACW', 'PHYS', 'PSTX', 'RBLX', 'RIVN', 'ROKU', 'RPHM', 'SCHW', 'SGHT', 'SNAP', 'STRO', 'TEAF', 'TSLA', 'UAL', 'UFAB', 'ULBI', 'VALE', 'WAL', 'XPEV', 'XTNT', 'YCBD']
     df = pd.DataFrame()
-    for (symbol, long_name) in longNames.items():
+    for symbol, long_name in longNames.items():
         # 1 yıllık verileri tickers.csv ye yaz
-        print('Symbol : ' + symbol)
-        ticker = Ticker(symbol=symbol, long_name=long_name, start_date='2022-03-01',
-                        end_date='2023-04-26')
+        print("Symbol : " + symbol)
+        ticker = Ticker(
+            symbol=symbol,
+            long_name=long_name,
+            start_date="2022-03-01",
+            end_date="2023-04-26",
+        )
         ticker.create_stock_data()
 
         df = pd.concat([df, ticker.get_stock_data()])
@@ -108,39 +114,46 @@ def continue_news():
     df = pd.read_csv("tickers.csv")
 
     existing_news = {}
-    with open('news_new.csv', 'r', encoding='utf-8') as file:
+    with open("news_new.csv", "r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         for row in reader:
-            existing_news[(row['date'], row['symbol'])] = True
+            existing_news[(row["date"], row["symbol"])] = True
 
-    x = (df['Date'], df['symbol'])
+    x = (df["Date"], df["symbol"])
     for i in range(len(x[0])):
         if (x[0][i], x[1][i]) not in existing_news:
             try:
-                print(x[0][i] + ' - ' + x[1][i])
+                print(x[0][i] + " - " + x[1][i])
                 tarih = x[0][i]
-                date_obj = datetime.strptime(tarih, '%Y-%m-%d')
-                yeni_tarih = date_obj.strftime('%m-%d-%Y')
+                date_obj = datetime.strptime(tarih, "%Y-%m-%d")
+                yeni_tarih = date_obj.strftime("%m-%d-%Y")
                 googlenews = GoogleNews(start=yeni_tarih, end=yeni_tarih)
-                googlenews.search('NASDAQ:' + x[1][i])
+                googlenews.search("NASDAQ:" + x[1][i])
                 # googlenews.search(x[1][i])
                 result = googlenews.result()
 
                 for item in result:
-                    title = item['title']
-                    text = item['desc']
-                    link = item['link']
-                    detailed_text = ''
+                    title = item["title"]
+                    text = item["desc"]
+                    link = item["link"]
+                    detailed_text = ""
                     try:
                         detailed_text = get_detailed_text_data(link)
                     except:
-                        detailed_text = 'NO_DETAILED_TEXT'
-                    news.append([title, text, link, x[0][i],
-                                x[1][i], detailed_text])
+                        detailed_text = "NO_DETAILED_TEXT"
+                    news.append([title, text, link, x[0][i], x[1][i], detailed_text])
             except TypeError as e_parser:
-                print('Type Error')
-                news.append(['NO_TITLE', 'NO_DESC', 'NO_LINK',
-                            x[0][i], x[1][i], 'NO_DETAILED_TEXT'])
+                print("Type Error")
+                news.append(
+                    [
+                        "NO_TITLE",
+                        "NO_DESC",
+                        "NO_LINK",
+                        x[0][i],
+                        x[1][i],
+                        "NO_DETAILED_TEXT",
+                    ]
+                )
             except:
                 break
 
@@ -152,34 +165,89 @@ def collab_google_trends_data():
     # nodejs ile toplanan google trend verilerini tek bir csv dosyasına at
     folder_path = "data/google_trends/trend_datas"
 
-    csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    csv_files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
 
     combined_csv = pd.concat(
-        [pd.read_csv(os.path.join(folder_path, f)) for f in csv_files])
+        [pd.read_csv(os.path.join(folder_path, f)) for f in csv_files]
+    )
 
-    combined_csv.to_csv("all_trend_datas.csv", index=False,
-                        encoding='utf-8-sig', header=['symbol', 'Date', 'value'])
+    combined_csv.to_csv(
+        "all_trend_datas.csv",
+        index=False,
+        encoding="utf-8-sig",
+        header=["symbol", "Date", "value"],
+    )
 
 
 def combine_tickers_with_trend_scores():
     # toplanan tek csvdeki trend verilerini kullanacağımız ana csv dosyası olan tickers.csv yle birleştir.
-    tickers_df = pd.read_csv('tickers.csv')
-    trends_df = pd.read_csv('all_trend_datas.csv')
+    tickers_df = pd.read_csv("tickers.csv")
+    trends_df = pd.read_csv("all_trend_datas.csv")
 
-    trends_df = trends_df[['symbol', 'Date', 'value']]
+    trends_df = trends_df[["symbol", "Date", "value"]]
 
-    merged_df = pd.merge(tickers_df, trends_df, on=[
-                         'Date', 'symbol'], how='left')
+    merged_df = pd.merge(tickers_df, trends_df, on=["Date", "symbol"], how="left")
 
-    merged_df['value'] = merged_df['value'].fillna(0)
+    merged_df["value"] = merged_df["value"].fillna(0)
 
-    merged_df = merged_df.rename(columns={'value': 'trend_score'})
-    merged_df.to_csv('tickers.csv', index=False)
-
+    merged_df = merged_df.rename(columns={"value": "trend_score"})
+    merged_df.to_csv("tickers.csv", index=False)
 
 
 
-#continue_news()
-#create_new_tickers_data()
-#collab_google_trends_data()
-combine_tickers_with_trend_scores()
+def create_daily_news_score(start_index):
+    df = pd.read_csv("tickers.csv")
+    news = pd.read_csv("news_new.csv")
+    ns = NewsScorer()
+    for index in range(start_index, len(df)):
+        #try:
+        print(index)
+        row = df.iloc[index]
+        symbol = row["symbol"]
+        date = row["Date"]
+        long_name = row["long_name"]
+        filtered_df = news[(news["symbol"] == symbol) & (news["date"] == date)]
+        titles = []
+        texts = []
+        dates = []
+        #toplam = 0
+        for index_nw, row_news in filtered_df.iterrows():
+            if row_news["link"] != "NO_LINK":
+                if not pd.isnull(row_news['title']):
+                    if (
+                        symbol
+                        in row_news["title"]
+                        + " "
+                        + row_news["text"]
+                        + " "
+                        + row_news["detailed_text"]
+                        or long_name
+                        in row_news["title"]
+                        + row_news["text"]
+                        + " "
+                        + row_news["detailed_text"]
+                    ):
+                        titles.append(row_news["title"])
+                        texts.append(row_news["text"] + " " + row_news["detailed_text"])
+                        dates.append(row_news["date"])
+                        #toplam += ns.sentiment_analysis(str(row_news["title"]) + ' ' + str(row_news["text"]) + ' ' + str(row_news["detailed_text"]))
+            
+        data = {
+            "date": dates,
+            "title": titles,
+            "text": texts,
+        }
+            
+        temp_df = pd.DataFrame(data)
+        ns.set_news_data(temp_df)
+        ns.create_scored_data()
+        print(ns.get_average_score()[date])
+        #print(toplam / len(filtered_df))
+        
+
+
+# continue_news()
+# create_new_tickers_data()
+# collab_google_trends_data()
+# combine_tickers_with_trend_scores()
+create_daily_news_score(0)
