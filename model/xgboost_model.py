0d30a39a8c86a51b0ff570453ebdbe20ee661684
Asım Kaymak <kaymakasm@gmail.com>
2023-05-01 03:25:06 +0300

    Update xgboost_model.py

--- model/xgboost_model.py
+++ model/xgboost_model.py
@@ -10,8 +10,9 @@ data = pd.read_csv('tickers.csv')
 data = data[(data["symbol"] == 'AAL')]
 data['Date'] = pd.to_datetime(data['Date'])
 data = data.set_index('Date')
+data = data.sort_values('Date')
 # Özellikler ve hedef değişken ayırma
-X = data[['trend_score', 'news_score_model3', 'Adj Close']]
+X = data[['trend_score', 'news_score_model1', 'Adj Close']]
 y = data['Adj Close']
 
 tarih = '2023-02-01'
@@ -63,12 +64,12 @@ shap.summary_plot(shap_values, X,plot_type="bar")
 shap.plots._waterfall.waterfall_legacy(expected_value, shap_values[79], features=X.loc[79,:], feature_names=X.columns, max_display=15, show=True)
 
 # Generate dependence plot
-shap.dependence_plot("worst concave points", shap_values, X, interaction_index="mean concave points")
+shap.dependence_plot("Adj Close", shap_values, X)
 
 # Generate multiple dependence plots
 for name in X_train.columns:
      shap.dependence_plot(name, shap_values, X)
-shap.dependence_plot("worst concave points", shap_values, X, interaction_index="mean concave points")
+shap.dependence_plot("Adj Close", shap_values, X)
 
 # Generate force plot - Multiple rows 
 shap.force_plot(explainer.expected_value, shap_values[:100,:], X.iloc[:100,:])
