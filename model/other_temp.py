c69a0c2af4c18b7757c81a6c774745ab9a86694f
Asım Kaymak <kaymakasm@gmail.com>
2023-05-04 03:04:03 +0300

    Update other_temp.py
    ElasticNET and ridge regression added

--- model/other_temp.py
+++ model/other_temp.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge, ElasticNet
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
 from sklearn.tree import DecisionTreeRegressor
@@ -13,17 +13,17 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import xgboost as xgb
 
 # Veri dosyasını oku
-data = pd.read_csv('tickers.csv')
+data = pd.read_csv("tickers.csv")
 
-data = data[(data["symbol"] == 'AAL')]
-data['Date'] = pd.to_datetime(data['Date'])
-data = data.set_index('Date')
-data = data.sort_values('Date')
+data = data[(data["symbol"] == "AAL")]
+data["Date"] = pd.to_datetime(data["Date"])
+data = data.set_index("Date")
+data = data.sort_values("Date")
 
 # Hedef değişken ayırma
-y = data['Adj Close']
+y = data["Adj Close"]
 
-tarih = '2023-02-01'
+tarih = "2023-02-01"
 
 # Eğitim ve test verileri ayırma
 y_train = y[y.index < tarih]
@@ -35,15 +35,15 @@ arima_fit = arima_model.fit()
 arima_forecast = arima_fit.forecast(steps=len(y_test))
 
 # ETS modeli
-ets_model = ExponentialSmoothing(y_train, trend='add', seasonal='add', seasonal_periods=12)
+ets_model = ExponentialSmoothing(
+    y_train, trend="add", seasonal="add", seasonal_periods=12
+)
 ets_fit = ets_model.fit()
 ets_forecast = ets_fit.forecast(steps=len(y_test))
 
 
-
-
 # Regression modelleri için veri özellikleri
-X = data[['trend_score', 'news_score_model3']]
+X = data[["trend_score", "news_score_model3", "Adj Close"]]
 X_train = X[X.index < tarih]
 X_test = X[X.index >= tarih]
 
@@ -53,7 +53,9 @@ rf_model.fit(X_train, y_train)
 rf_forecast = rf_model.predict(X_test)
 
 # Gradient Boosting modeli
-gb_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0)
+gb_model = GradientBoostingRegressor(
+    n_estimators=100, learning_rate=0.1, max_depth=1, random_state=0
+)
 gb_model.fit(X_train, y_train)
 gb_forecast = gb_model.predict(X_test)
 
@@ -63,7 +65,7 @@ lr_model.fit(X_train, y_train)
 lr_forecast = lr_model.predict(X_test)
 
 # Support Vector Regression modeli
-svr_model = SVR(kernel='linear')
+svr_model = SVR(kernel="linear")
 svr_model.fit(X_train, y_train)
 svr_forecast = svr_model.predict(X_test)
 
@@ -77,24 +79,41 @@ knn_model = KNeighborsRegressor(n_neighbors=5)
 knn_model.fit(X_train, y_train)
 knn_forecast = knn_model.predict(X_test)
 
-#XGB modeli
-xgb_model = xgb.XGBRegressor(objective='reg:squarederror', colsample_bytree=0.3, learning_rate=0.1,
-                max_depth=10, alpha=10, n_estimators=100)
+# XGB modeli
+xgb_model = xgb.XGBRegressor(
+    objective="reg:squarederror",
+    colsample_bytree=0.3,
+    learning_rate=0.1,
+    max_depth=10,
+    alpha=10,
+    n_estimators=100,
+)
 xgb_model.fit(X_train, y_train)
 xgb_forecast = xgb_model.predict(X_test)
 
+# Ridge
+ridge_model = Ridge(alpha=1)
+ridge_model.fit(X_train, y_train)
+ridge_forecast = ridge_model.predict(X_test)
+
+# ElasticNET
+elasticnet_model = ElasticNet(alpha=1, l1_ratio=0.5)
+elasticnet_model.fit(X_train, y_train)
+elasticnet_forecast = elasticnet_model.predict(X_test)
 # Tahminlerin performansını değerlendirme
 models = {
-    'ARIMA': arima_forecast,
-    'ETS': ets_forecast,
-    'Random Forest': rf_forecast,
-    'Gradient Boosting': gb_forecast,
-    'Linear Regression': lr_forecast,
-    'Support Vector Regression': svr_forecast,
-    'Decision Tree': dt_forecast,
-    'K-Nearest Neighbors': knn_forecast
+    "ARIMA": arima_forecast,
+    "ETS": ets_forecast,
+    "Random Forest": rf_forecast,
+    "Gradient Boosting": gb_forecast,
+    "Linear Regression": lr_forecast,
+    "Support Vector Regression": svr_forecast,
+    "Decision Tree": dt_forecast,
+    "K-Nearest Neighbors": knn_forecast,
+    "XGBoost": xgb_forecast,
+    "Ridge Regression": ridge_forecast,
+    "ElasticNet": elasticnet_forecast,
 }
-models['XGBoost'] = xgb_forecast
 
 for model_name, forecast in models.items():
     mse = mean_squared_error(y_test, forecast)
