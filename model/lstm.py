37be4e6446c29d8e859154642aa417e42474f8a0
Asım Kaymak <kaymakasm@gmail.com>
2023-05-23 12:52:00 +0300

    lstm and rnn added

--- model/lstm.py
+++ model/lstm.py
@@ -4,70 +4,103 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+
+def lstm_model(time_steps,tarih,symbol,parameters):
 
 # Veri dosyasını oku
-data = pd.read_csv("tickers.csv")
+    data = pd.read_csv("tickers.csv")
 
-data = data[(data["symbol"] == "AAL")]
-data["Date"] = pd.to_datetime(data["Date"])
-data = data.set_index("Date")
-data = data.sort_values("Date")
+    data = data[(data["symbol"] == symbol)]
+    data["Date"] = pd.to_datetime(data["Date"])
+    data = data.set_index("Date")
+    data = data.sort_values("Date")
 
-# Hedef değişken ayırma
-y = data["Adj Close"]
-
-tarih = "2023-02-01"
-
-# Eğitim ve test verileri ayırma
-y_train = y[y.index < tarih]
-y_test = y[y.index >= tarih]
-
-# Regression modelleri için veri özellikleri
-X = data[["trend_score", "news_score_model1"]]
-X_train = X[X.index < tarih]
-X_test = X[X.index >= tarih]
-
-# LSTM için veri önişleme
-n_steps = 5
-X_train_lstm = np.array([X_train.values[i-n_steps:i, :] for i in range(n_steps, len(X_train))])
-y_train_lstm = y_train.values[n_steps:]
-X_test_lstm = np.array([X_test.values[i-n_steps:i, :] for i in range(n_steps, len(X_test))])
-y_test_lstm = y_test.values[n_steps:]
-
-# Verileri LSTM için normalleştirme
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data['Volume']= scaler.fit_transform(data[['Volume']])
+    data['Open']= scaler.fit_transform(data[['Open']])
+    data['High']= scaler.fit_transform(data[['High']])
+    data['Low']= scaler.fit_transform(data[['Low']])
+    data['trend_score']= scaler.fit_transform(data[['trend_score']])
+    data['news_score_model1']= scaler.fit_transform(data[['news_score_model1']])
+    data['news_score_model3']= scaler.fit_transform(data[['news_score_model3']])
+    data['news_score_model2']= scaler.fit_transform(data[['news_score_model2']])
+    # Hedef değişken ayırma
+    y_lstm = data["Adj Close"]
 
 
-# LSTM modelini oluşturma
-lstm_model = Sequential()
-lstm_model.add(LSTM(50, activation='relu', input_shape=(n_steps, X_train.shape[1])))
-lstm_model.add(Dense(1))
-lstm_model.compile(optimizer='adam', loss='mse')
+    # Eğitim ve test verileri ayırma
+    y_train_1 = y_lstm[y_lstm.index < tarih]
+    y_test_1 = y_lstm[y_lstm.index >= tarih]
 
-# LSTM modelini eğitme
-lstm_model.fit(X_train_lstm, y_train_lstm, epochs=10, verbose=0)
+    # Regression modelleri için veri özellikleri
+    X_lstm = data[parameters]
+    X_train_1 = X_lstm[X_lstm.index < tarih]
+    X_test_1 = X_lstm[X_lstm.index >= tarih]
 
-# LSTM ile tahmin yapma
-lstm_forecast = lstm_model.predict(X_test_lstm)
 
-# Tahminlerin performansını değerlendirme
-models = {
-    "LSTM": lstm_forecast,  # LSTM modelini ekle
-}
 
-for model_name, forecast in models.items():
-    mse = mean_squared_error(y_test_lstm, forecast)
-    print(f"{model_name} modeli için Test Kümesi Ortalama Kare Hatası: {mse:.2f}")
+    # LSTM için veri önişleme
+    n_steps = 5
+    # Verileri LSTM için normalleştirme
 
-# Tahminleri grafikle gösterme
-plt.figure(figsize=(15, 8))
 
-for model_name, forecast in models.items():
-    plt.plot(y_test.index[n_steps:], forecast, label=model_name)
 
-plt.plot(y_test.index, y_test, label='Gerçek Değerler', color='black', linewidth=2)
-plt.xlabel('Date')
-plt.ylabel('Adj Close')
-plt.title('Tahminler ve Gerçek Değerler')
-plt.legend()
-plt.show()
+
+
+
+    def create_dataset(X, y, time_steps=1):
+        Xs, ys = [], []
+        for i in range(len(X) - time_steps):
+            v = X.iloc[i:(i + time_steps)].values
+            Xs.append(v)  
+            ys.append(y.iloc[i + time_steps])
+        return np.array(Xs), np.array(ys)
+
+    
+
+    # reshape to [samples, time_steps, n_features]
+    X_train_lstm, y_train_lstm = create_dataset(X_train_1, y_train_1, time_steps)
+    X_test_lstm, y_test_lstm = create_dataset(X_test_1, y_test_1, time_steps)
+
+    print(X_train_lstm.shape, y_train_lstm.shape)
+
+    lstm_model = Sequential()
+    lstm_model.add(LSTM(64, activation='relu', input_shape=(X_train_lstm.shape[1], X_train_lstm.shape[2])))
+    lstm_model.add(Dropout(0.2))
+    lstm_model.add(Dense(1))
+    lstm_model.compile(optimizer='adam', loss='mse')
+
+    history = lstm_model.fit(
+        X_train_lstm, y_train_lstm, 
+        epochs=30, 
+        batch_size=32, 
+        validation_split=0.1,
+        shuffle=False
+    )
+
+    lstm_forecast = lstm_model.predict(X_test_lstm)
+
+    return [lstm_forecast,y_test_lstm,y_test_1,time_steps]
+    # Tahminlerin performansını değerlendirme
+    models = {
+        "LSTM": lstm_forecast,  # LSTM modelini ekle
+    }
+
+    for model_name, forecast in models.items():
+        mse = mean_squared_error(y_test_lstm, forecast)
+        print(f"{model_name} modeli için Test Kümesi Ortalama Kare Hatası: {mse:.2f}")
+
+    # Tahminleri grafikle gösterme
+    plt.figure(figsize=(15, 8))
+
+    for model_name, forecast in models.items():
+        plt.plot(y_test_1.index[time_steps:], forecast, label=model_name)  # y_test.index[n_steps:] -> y_test.index[time_steps:]
+
+    plt.plot(y_test_1.index[time_steps:], y_test_1[time_steps:], label='Gerçek Değerler', color='black', linewidth=2)  # y_test.index -> y_test.index[time_steps:], y_test -> y_test[time_steps:]
+    plt.xlabel('Date')
+    plt.ylabel('Adj Close')
+    plt.title('Tahminler ve Gerçek Değerler')
+    plt.legend()
+    plt.show()
--- model/rnn.py
+++ model/rnn.py
@@ -1,87 +1,73 @@
 import pandas as pd
 import numpy as np
-import xgboost as xgb
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
-from sklearn.linear_model import LinearRegression, Ridge, ElasticNet
-from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
-from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
-import xgboost as xgb
-import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, SimpleRNN, Dropout
+from tensorflow.keras.layers import SimpleRNN, Dense, Dropout
 
-from sklearn.metrics import accuracy_score
-import os
-import time
+def rnn_model(time_steps, tarih, symbol, parameters):
+    # Veri dosyasını oku
+    data = pd.read_csv("tickers.csv")
 
-# Veri dosyasını oku
-data = pd.read_csv("tickers.csv")
+    data = data[(data["symbol"] == symbol)]
+    data["Date"] = pd.to_datetime(data["Date"])
+    data = data.set_index("Date")
+    data = data.sort_values("Date")
 
-data = data[(data["symbol"] == "TSLA")]
-data["Date"] = pd.to_datetime(data["Date"])
-data = data.set_index("Date")
-data = data.sort_values("Date")
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    data['Volume'] = scaler.fit_transform(data[['Volume']])
+    data['Open'] = scaler.fit_transform(data[['Open']])
+    data['High'] = scaler.fit_transform(data[['High']])
+    data['Low'] = scaler.fit_transform(data[['Low']])
+    data['trend_score']= scaler.fit_transform(data[['trend_score']])
+    data['news_score_model1']= scaler.fit_transform(data[['news_score_model1']])
+    data['news_score_model3']= scaler.fit_transform(data[['news_score_model3']])
+    data['news_score_model2']= scaler.fit_transform(data[['news_score_model2']])
+    # Hedef değişken ayırma
+    y_rnn = data["Adj Close"]
 
-# Hedef değişken ayırma
-y = data["Adj Close"]
+    # Eğitim ve test verileri ayırma
+    y_train_1 = y_rnn[y_rnn.index < tarih]
+    y_test_1 = y_rnn[y_rnn.index >= tarih]
 
-tarih = "2023-02-01"
+    # Regression modelleri için veri özellikleri
+    X_rnn = data[parameters]
+    X_train_1 = X_rnn[X_rnn.index < tarih]
+    X_test_1 = X_rnn[X_rnn.index >= tarih]
 
-# Eğitim ve test verileri ayırma
-y_train = y[y.index < tarih]
-y_test = y[y.index >= tarih]
+    # RNN için veri önişleme
+    n_steps = 5
+    # Verileri RNN için normalleştirme
 
-X = data[["trend_score", "news_score_model3"]]
-X_train = X[X.index < tarih]
-X_test = X[X.index >= tarih]
+    def create_dataset(X, y, time_steps=1):
+        Xs, ys = [], []
+        for i in range(len(X) - time_steps):
+            v = X.iloc[i:(i + time_steps)].values
+            Xs.append(v)
+            ys.append(y.iloc[i + time_steps])
+        return np.array(Xs), np.array(ys)
 
-# RNN
-rnn_units = 128
-dense_units = 64
-optimizer = 'RMSprop'
-epochs = 200
-batch_size = 16
+    # reshape to [samples, time_steps, n_features]
+    X_train_rnn, y_train_rnn = create_dataset(X_train_1, y_train_1, time_steps)
+    X_test_rnn, y_test_rnn = create_dataset(X_test_1, y_test_1, time_steps)
 
-def train_rnn_model():
+    print(X_train_rnn.shape, y_train_rnn.shape)
+
     rnn_model = Sequential()
-    rnn_model.add(SimpleRNN(rnn_units, input_shape=(X_train.shape[1], 1), activation='tanh'))
-    rnn_model.add(Dense(dense_units))
-    rnn_model.compile(optimizer=optimizer, loss='mse')
-    X_train_rnn = X_train.values.reshape(X_train.shape[0], X_train.shape[1], 1)
-    X_test_rnn = X_test.values.reshape(X_test.shape[0], X_test.shape[1], 1)
-    rnn_model.fit(X_train_rnn, y_train, epochs=epochs, batch_size=batch_size)
+    rnn_model.add(SimpleRNN(64, activation='relu', input_shape=(X_train_rnn.shape[1], X_train_rnn.shape[2])))
+    rnn_model.add(Dropout(0.2))
+    rnn_model.add(Dense(1))
+    rnn_model.compile(optimizer='adam', loss='mse')
+
+    history = rnn_model.fit(
+        X_train_rnn, y_train_rnn,
+        epochs=30,
+        batch_size=32,
+        validation_split=0.1,
+        shuffle=False
+    )
+
     rnn_forecast = rnn_model.predict(X_test_rnn)
-    return rnn_model, rnn_forecast
 
-model_dosya_adi = "rnn_model_tsla.h5"
-yeni_mse = 10000
-
-while yeni_mse > 0.5:
-    time.sleep(1)
-    rnn_model, rnn_forecast = train_rnn_model()
-    X_test_rnn = X_test.values.reshape(X_test.shape[0], X_test.shape[1], 1)
-    if os.path.exists(model_dosya_adi):
-        mevcut_model = tf.keras.models.load_model(model_dosya_adi)
-        mevcut_model.evaluate(X_test_rnn, y_test, verbose=0)
-        mevcut_mse = mevcut_model.evaluate(X_test_rnn, y_test, verbose=0)
-
-        yeni_mse = rnn_model.evaluate(X_test_rnn, y_test, verbose=0)
-        print("Yüklenen Modelinin Ortalama Karesel Hata (MSE):", mevcut_mse)
-        print("Yeni Modelin Ortalama Karesel Hata (MSE):", yeni_mse)
-
-        if yeni_mse < mevcut_mse:
-            rnn_model.save(model_dosya_adi)
-            print("Yeni model, mevcut kaydedilen modele göre daha iyi. Yeni model kaydedildi.")
-        else:
-            print("Yeni model, mevcut kaydedilen modele göre daha kötü veya aynı performans. Yeni model kaydedilmedi.")
-    else:
-        rnn_model.save(model_dosya_adi)
-        mse = rnn_model.evaluate(X_test_rnn, y_test, verbose=0)
-        print("RNN Modelinin Ortalama Karesel Hata (MSE):", mse)
-        print("İlk kez çalıştırılıyor. Yeni model kaydedildi.")
+    return [rnn_forecast, y_test_rnn, y_test_1, time_steps]
