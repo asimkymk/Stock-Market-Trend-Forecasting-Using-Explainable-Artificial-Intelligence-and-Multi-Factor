9470f0e3f65ecec16901646ff8a1df5eb9200283
Asım Kaymak <kaymakasm@gmail.com>
2023-05-27 17:31:55 +0300

    deleted unnecessary files

--- test.py
+++
@@ -1,3 +0,0 @@
-from model.calculate_all_models import  calculate_all_models
-from model.convert_to_lime import plot_lime_models
-plot_lime_models("AAL",tarih= '2023-02-01',useTrend=False,news_model=False,delay=2)
