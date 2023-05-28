3175498bdbfd54ee52453dfbf7f60f666b499e5b
Asım Kaymak <kaymakasm@gmail.com>
2023-05-29 01:28:17 +0300

    Update page.tsx

--- frontend/marketapp/src/app/page.tsx
+++ frontend/marketapp/src/app/page.tsx
@@ -262,7 +262,7 @@ const YourComponent = () => {
                 </div>
                 <div className="flex-col ml-5 mr-5">
                   <div className="">
-                    <label htmlFor="newsScoreOption" className="mr-2">
+                    <label htmlFor="newsScoreOption" className="mr-2 text-sm font-medium text-white">
                       Select News Score Model:
                     </label>
                   </div>
@@ -283,7 +283,7 @@ const YourComponent = () => {
                 </div>
                 <div className="flex items-center mt-4">
                   <input id="addOpenValue" onChange={handleCheckboxChange} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                    <label htmlFor="addOpenValue" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Use Trend Score factor</label>
+                    <label htmlFor="addOpenValue" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300 ">Use Trend Score factor</label>
                 </div>
 
                 
